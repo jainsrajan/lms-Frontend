32e625fd2d305af64103dfda4b459eddcfc6e41c
Srajan jain <110524518+jainsrajan@users.noreply.github.com>
2024-06-04 23:37:02 +0530

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -8,11 +8,11 @@ function HomePage()
     return (
         <HomeLayout>
 
-           <div className="pt-10 text-gray flex items-center justify-center gap-20 h-[90vh]">
+           <div className="pt-10 text-gray flex items-center justify-center gap-20 h-[90vh] xs:flex-col">
             
             <div className="w-1/2 space-y-6">
 
-            <h1 className="text-5xl font-semibold  text-white">
+            <h1 className="text-5xl font-semibold  text-white xs:text-4xl mt-24">
 
             Find out Best
            <span className="text-yellow-500 font-bold">
@@ -21,18 +21,18 @@ function HomePage()
 
             </h1>
  
-<p className="text-xl text-gray-200 ">
+<p className="text-xl text-gray-200 xs:text-2xl ">
 
 We have a large library of course taught by highly skilled and qualified faculties at a very affordable cost.
 
 </p>
 
 
-<div className="space-x-6">
+<div className="space-x-6 xs:flex flex-row gap-5 items-center justify-center">
 
     <Link to="/courses">
 
-<button className="bg-yellow-500 px-4 py-2 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-3000">
+<button className="bg-yellow-500 px-4 py-2 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-3000 xs:px-8">
 Explor Courses
 </button>
 
@@ -40,7 +40,7 @@ Explor Courses
 
     <Link to="/contact">
 
-<button className="border border-yellow-500 px-4 py-2 rounded-md font-semibold text-lg text-white cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-3000">
+<button className="border border-yellow-500 px-4 py-2 rounded-md font-semibold text-lg text-white cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-3000 xs:px-8">
 Contact Us
 </button>
 
@@ -53,7 +53,7 @@ Contact Us
 
 <div className="w-1/3 flex items-center justify-center ml-5">
 
-<img src = {HomePageImage} className="mb-10" alt = 'homepage' />
+<img src = {HomePageImage} className="mb-10 xs:hidden" alt = 'homepage' />
 
 </div>
 
