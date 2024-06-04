db90cca01e4226534a1533e7932afa303bef94cf
Srajan jain <110524518+jainsrajan@users.noreply.github.com>
2024-06-04 23:52:55 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,30 @@ export default {
 ],
 
   theme: {
-    extend: {},
+ screens: {
+   
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    
+    
+    extend: {
+       screens:{
+        'xs':{'min':'320px', 'max':'640px'}
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
